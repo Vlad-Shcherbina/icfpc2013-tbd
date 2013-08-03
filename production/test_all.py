4cdb4a9834466f73130fdd857e9968ca546cb208
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2013-08-03 16:21:30 +0400

    test_all cleanup

--- production/test_all.py
+++ production/test_all.py
@@ -1,11 +1,11 @@
 import sys
-import logging
 
 import nose
 
 
 if __name__ == '__main__':
     nose.run(argv=sys.argv + [
-        '--verbose', '--with-doctest', '--with-coverage',
+        '--verbose', '--with-doctest',
+        #'--with-coverage', '--cover-package=production',
         '--logging-level=DEBUG'
         ])
